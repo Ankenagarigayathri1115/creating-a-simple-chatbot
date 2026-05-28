5641b3ca6f19b2fc489b196935f83ac5d7bd9025
Ankenagarigayathri1115 <ankenagarigayathri37@gmail.com>
2026-05-28 12:29:32 +0530

    Add files via upload

---
+++ Simple Chatbot using Python (CLI).py
@@ -0,0 +1,29 @@
+# Simple Chatbot using Python (CLI)
+
+print("===== Simple Chatbot =====")
+print("Type 'bye' to exit.\n")
+
+while True:
+
+    # User input
+    user = input("You: ").lower()
+
+    # Chatbot responses
+    if user == "hello" or user == "hi":
+        print("Bot: Hello! How can I help you?")
+
+    elif user == "how are you":
+        print("Bot: I am fine. Thank you!")
+
+    elif user == "what is your name":
+        print("Bot: I am a simple Python chatbot.")
+
+    elif user == "who created you":
+        print("Bot: I was created using Python.")
+
+    elif user == "bye":
+        print("Bot: Goodbye! Have a nice day.")
+        break
+
+    else:
+        print("Bot: Sorry, I don't understand that.")
